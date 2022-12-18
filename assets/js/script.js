b7c85009364d2722ff88be3e2cef43195a63fba7
GeeksforGeeks <ahfateme79@gmail.com>
2022-12-18 07:28:21 -0800

    add input to reserv section

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,6 +30,7 @@ function menuhandle() {
 // tab
 let tab = document.querySelectorAll('.tab ul li')
 let content = document.querySelectorAll('.content')
+let day_inp = document.querySelector("#days")
 
 
 tab.forEach(n => {
@@ -41,7 +42,6 @@ tab.forEach(n => {
             content[d].classList.remove('active')
         }
         n.classList.add('active')
-
         let id = e.target.dataset.name
         document.getElementById(id).classList.add('active')
     })
@@ -54,12 +54,30 @@ function beforeAfter() {
 }
 
 
+
+// hour
+
+let hour_content = document.querySelectorAll('.hour-content p label')
+let hour_inp = document.querySelector("#hour")
+
+hour_content.forEach(n => {
+    n.addEventListener('click', () => {
+        hour_inp.value = n.innerHTML
+    })
+})
+
+
+
+
 // week
 let week = document.querySelectorAll('.week ul li')
 let hour = document.querySelectorAll('.hour-content')
 
 
 week.forEach(n => {
+    if (n.classList.contains('active')) {
+        day_inp.value = n.innerHTML
+    }
     n.addEventListener('click', (e) => {
         for (let i = 0; i < week.length; i++) {
             week[i].classList.remove('active')
@@ -68,23 +86,12 @@ week.forEach(n => {
             hour[d].classList.remove('active')
         }
         n.classList.add('active')
+        if (n.classList.contains('active')) {
+            day_inp.value = n.innerHTML
+        }
 
         let id = e.target.dataset.name
         document.getElementById(id).classList.add('active')
-    })
-})
-
-
-
-
-// hour
-
-let hour_content = document.querySelectorAll('.hour-content p label')
-let hour_inp = document.querySelector(".hour-inp input")
-
-hour_content.forEach(n => {
-    n.addEventListener('click', () => {
-        hour_inp.value = n.innerHTML
     })
 })
 
